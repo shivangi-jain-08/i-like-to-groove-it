97effa071ee0b0f01f78afb28480af4f996b24c7
shivangi-jain-08 <shivangi.jain@kalvium.community>
2023-08-25 12:33:30 +0530

    done

---
+++ app.js
@@ -0,0 +1,6 @@
+var button = document.getElementById("buy")
+
+button.onclick=()=>{
+    button.style.display = "none";
+    document.getElementById("message").textContent = "Thank You For Shopping With Us."
+}
